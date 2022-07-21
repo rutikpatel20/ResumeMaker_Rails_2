2468654c79ba2fcf026c4f7ddbaea23716e76720
rutikpatel20 <rutik.p@simformsolutions.com>
2022-07-21 11:01:41 +0530

    validation added in the form

--- app/models/hobby2.rb
+++ app/models/hobby2.rb
@@ -1,3 +1,5 @@
 class Hobby2 < ApplicationRecord
   belongs_to :resume2, optional: true
+
+  validates :hobby2, presence: true
 end
--- app/models/skill2.rb
+++ app/models/skill2.rb
@@ -1,3 +1,5 @@
 class Skill2 < ApplicationRecord
   belongs_to :resume2, optional: true
+
+  validates :skill2, presence: true
 end
