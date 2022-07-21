347d35c1d9c532915253e8ca200d6e171a85d022
rutikpatel20 <rutik.p@simformsolutions.com>
2022-07-21 12:24:13 +0530

    validation added in the form

--- app/models/experience3.rb
+++ app/models/experience3.rb
@@ -1,3 +1,9 @@
 class Experience3 < ApplicationRecord
   belongs_to :resume3, optional: true
+
+  validates :job_title3, presence: true
+  validates :employer3, presence: true
+  validates :start_date3, presence: true
+  validates :end_date3, presence: true
+  validates :work_description3, presence: true, length: { minimum: 260 }
 end
